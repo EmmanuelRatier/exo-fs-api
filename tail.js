5916cfd3dc2cb6dd2019f74f1d9679d89b3ecf88
EmmanuelRatier <ratier.e09@gmail.com>
2021-03-25 22:45:30 +0100

    ok but do the checks

--- tail.js
+++ tail.js
@@ -1,19 +1,16 @@
 const fs = require('fs')
-let elem = process.argv[2]
-if (process.argv.length !== 3) {
+
+if (process.argv.length !== 3 && process.argv.length !== 5) {
   console.log('pas le bon nombre d\'argument')
 }
-if (!fs.existsSync(elem)) {//verifier si le fichier existe
-  console.log(`error: ${elem} does not exist`)
-  process.exit(1)
-}
-const stats = fs.statSync(elem)// verifie si c'est un fichier
-if (!stats.isFile()) {
-  console.log(`Error: ${elem} is not a file`)
-  process.exit(1)
-}
-else {
+if (process.argv.length === 3) {
   let textFile = fs.readFileSync(process.argv[2], 'utf-8')
-  let lastTenLine = textFile.split('\n').splice(-11, 10)
+  let lastTenLine = textFile.split('\n').splice(-10, 10)
   console.log(lastTenLine.join('\n'))
+} else if (process.argv.length === 5 && process.argv[2] === '-n') {
+  let numberParam = process.argv[3]
+  let textFileWithParam = fs.readFileSync(process.argv[4], 'utf-8')
+  let lastLine = textFileWithParam.split('\n').splice(- numberParam, numberParam)
+  //console.log(lastLine.length)
+  console.log(lastLine.join('\n'))
 }
